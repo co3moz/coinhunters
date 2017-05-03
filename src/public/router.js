44ac043ed31b57523df1effa5e4692b812d95de4
Doğan Derya <doganderya59@gmail.com>
2017-05-03 14:37:59 +0300

    very nice

--- src/public/router.js
+++ src/public/router.js
@@ -2,18 +2,33 @@ var container = document.getElementById('container');
 document.body.style.backgroundColor = '#2c3338';
 document.body.style.color = '#606468';
 
-window.navigatePage = function (page) {
+window.navigatePage = function (page, fn) {
     window.location.hash = page;
 
-    getRequest('/page/' + page + '.html', function (html) {
+    var subPages = page.split('/');
+
+    getRequest('/page/' + subPages[0] + '.html', function (html) {
+        window.pageParts = subPages;
         container.innerHTML = html;
+        Array.from(container.getElementsByTagName('script')).forEach(function (e) {
+            if (!e.getAttribute('executed')) {
+                eval(e.innerHTML);
+                e.setAttribute('executed', 'true');
+            }
+        })
+        if (fn) fn();
     });
 }
 
 var last;
 setInterval(function () {
     if (window.location.hash != last) {
+        console.log(last, window.location.hash);
         navigatePage(window.location.hash.substring(1));
         last = window.location.hash;
     }
-}, 500);
+}, 250);
+
+if (window.location.hash == "" || window.location.hash == "#") {
+    window.location.hash = "#login";
+}
--- src/public/utils.js
+++ src/public/utils.js
@@ -1,13 +1,27 @@
+var token = localStorage.getItem('token');
+if (token != null && token.constructor != String) {
+    token = "";
+}
+
+window.setToken = function setToken(newToken) {
+    token = newToken;
+    localStorage.setItem('token', newToken);
+}
+
 window.basicRequest = function basicRequest(method, url, data, fn) {
     var xhr = new XMLHttpRequest();
     xhr.open(method, url, true);
     xhr.setRequestHeader('Content-Type', 'application/json');
-    if (data.token) {
-        xhr.setRequestHeader('Token', data.token);
+    if (token) {
+        xhr.setRequestHeader('Token', token);
     }
-    xhr.send(JSON.stringify(data.body));
+    if (data) {
+        xhr.send(JSON.stringify(data));
+    } else {
+        xhr.send();
+    }
     xhr.onreadystatechange = function () {
-        if (this.readyState == 4) {
+        if (this.readyState == 4 && this.status == 200) {
             var data = xhr.response;
             try {
                 data = JSON.parse(data);
@@ -17,10 +31,39 @@ window.basicRequest = function basicRequest(method, url, data, fn) {
             }
 
             fn(data);
+        } else if (this.readyState == 4 && (this.status == 401 || this.status == 408)) {
+            if (window.location.hash != "#login") {
+                navigatePage('login');
+            }
         }
     }
 }
 
 window.getRequest = function getRequest(url, fn) {
-    basicRequest('GET', url, {}, fn);
+    basicRequest('GET', url, null, fn);
+}
+
+window.postRequest = function postRequest(url, data, fn) {
+    basicRequest('POST', url, data, fn);
+}
+
+window.waitMultiple = function () {
+    var args = Array.from(arguments);
+    if (args.length < 2) {
+        throw new Error('provide at least one function with end function')
+    }
+
+    var count = 0;
+    var datas = [];
+    function finished(data) {
+        count++;
+        datas[this] = data;
+        if (count == args.length - 1) {
+            args[args.length - 1].apply(null, datas);
+        }
+    }
+
+    for (var i = 0; i < args.length - 1; i++) {
+        args[i](finished.bind(i));
+    }
 }
