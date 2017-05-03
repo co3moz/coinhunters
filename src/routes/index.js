b171af950a83a90cc59a590b6ec24cadf4fc5508
Doğan Derya <doganderya59@gmail.com>
2017-05-03 12:23:02 +0300

    some importants

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 const gluon = require('gluon');
 
 const router = gluon.router();
+const path = require('path')
 
 router.use('/', function (req, res) {
- res.notFound('unknown service');
+    res.sendFile(path.resolve(__dirname, '../build/index.html'));
 });
 
 module.exports = router;
